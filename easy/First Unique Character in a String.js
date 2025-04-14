d885b96ab15b3ed539900279e336f7f4da676396
NamTNDEV <namtndev312002@gmail.com>
2025-04-14 09:26:49 +0700

    [#19] First Unique Character in a String wi Map Object Solution

--- easy/First Unique Character in a String.js
+++ easy/First Unique Character in a String.js
@@ -11,3 +11,17 @@ var firstUniqChar = function (s) {
 
   return -1;
 };
+
+var firstUniqChar2 = function (s) {
+  const counts = new Map();
+
+  for (let char of s) {
+    counts.set(char, (counts.get(char) || 0) + 1);
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    if (counts.get(s[i]) === 1) return i;
+  }
+
+  return -1;
+};
